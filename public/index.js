1a03aee86510e77260cdd09f2d8a99af42b06feb
annalanigan <annalanigan@outlook.com>
2018-01-31 14:24:21 +0000

    country details displayed

--- public/index.js
+++ public/index.js
@@ -8,11 +8,29 @@ var makeRequest = function(url, callback){
 
 var populateList = function(countries){
   var select = document.querySelector('#drop-down');
-  countries.forEach(function(country){
+
+  countries.forEach(function(country, index){
     var option = document.createElement('option');
     option.innerText = country.name;
+    option.value = index;
     select.appendChild(option);
   });
+
+  select.addEventListener('change', function(){
+    var li = document.createElement('li');
+    var h3 = document.createElement('h3');
+    h3.innerText = "Country " + countries[select.value].name;
+    var h5 = document.createElement('h5');
+    h5.innerText = "Population " + countries[select.value].population;
+    var p = document.createElement('p');
+    p.innerText = "Capital " + countries[select.value].capital;
+    var ul = document.querySelector('#country-list')
+    li.appendChild(h3);
+    li.appendChild(h5);
+    li.appendChild(p);
+    ul.appendChild(li);
+  })
+
 }
 
 
@@ -21,7 +39,6 @@ var requestComplete = function(){
   if(this.status !== 200) {
     return;
   }
-
   var jsonString = this.responseText;
   var countries = JSON.parse(jsonString);
   populateList(countries);
